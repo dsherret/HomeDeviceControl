8b1de1ce61cfe4ccd055177e95b23c98db700c57
David Sherret <dsherret@gmail.com>
2018-08-12 16:45:39 -0400

    Add WindowsEventService for listening to sending computer power events on windows.
    Unfortunately I couldn't use the Task Scheduler in windows because it will send a sleep event the moment it goes to sleep so the command won't happen until the computer wakes up.

---
+++ DeviceControl.WindowsEventService/EventService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ServiceProcess;
+
+namespace DeviceControl.WindowsEventService
+{
+    public partial class EventService : ServiceBase
+    {
+        public EventService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
+        }
+
+        protected override void OnStop()
+        {
+            SystemEvents.PowerModeChanged -= SystemEvents_PowerModeChanged;
+        }
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Resume:
+                    SendPowerStatus(false);
+                    break;
+                case PowerModes.Suspend:
+                    SendPowerStatus(true);
+                    break;
+            }
+        }
+
+        private async void SendPowerStatus(bool isPoweredOn)
+        {
+            // todo: don't hardcode
+            const string computerDeviceId = "7d115c0c-6181-4965-bceb-449781ecd27a";
+            const string serverUrl = "http://192.168.1.126:8084";
+
+            using (var client = new Communication.ClientApi.Client(serverUrl))
+                await client.UpdateDevicePowerStatus(new Guid(computerDeviceId), isPoweredOn);
+        }
+    }
+}
