e6701d96ea807439ce12c0732b92f3a17c558c23
David Sherret <dsherret@gmail.com>
2018-08-12 22:09:39 -0400

    Needed to use a hidden form in order to get a message loop to receive the power mode changed events.

--- DeviceControl.WindowsEventService/EventService.cs
+++ DeviceControl.WindowsEventService/EventService.cs
@@ -1,8 +1,7 @@
 ﻿using DeviceControl.Core;
-using Microsoft.Win32;
-using System;
 using System.ServiceProcess;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace DeviceControl.WindowsEventService
 {
@@ -26,56 +25,20 @@ namespace DeviceControl.WindowsEventService
         protected async override void OnStart(string[] args)
         {
             Logger.Log(this, LogLevel.Info, "Service starting...");
-            await SendPowerStatusAsync(true);
-            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
+            await PowerStatus.SendAsync(true);
+            new Thread(RunMessagePump).Start();
             Logger.Log(this, LogLevel.Info, "Service started.");
+        }
+
+        private void RunMessagePump()
+        {
+            Application.Run(new HiddenForm());
         }
 
         protected override void OnStop()
         {
-            SystemEvents.PowerModeChanged -= SystemEvents_PowerModeChanged;
             Logger.Log(this, LogLevel.Info, "Service stopped.");
-        }
-
-        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
-        {
-            try
-            {
-                // wait on the task to ensure the computer doesn't go into sleep mode before it's done
-                SendStatusAsync().Wait();
-            }
-            catch (Exception ex)
-            {
-                Logger.Log(this, LogLevel.Error, ex);
-                throw ex;
-            }
-
-            async Task SendStatusAsync()
-            {
-                switch (e.Mode)
-                {
-                    case PowerModes.Resume:
-                        await SendPowerStatusAsync(true);
-                        break;
-                    case PowerModes.Suspend:
-                        await SendPowerStatusAsync(false);
-                        break;
-                }
-            }
-        }
-
-        private async Task SendPowerStatusAsync(bool isPoweredOn)
-        {
-            // todo: don't hardcode
-            const string computerDeviceId = "7d115c0c-6181-4965-bceb-449781ecd27a";
-            const string serverUrl = "http://192.168.1.125:8084";
-
-            Logger.Log(this, LogLevel.Info, $"Sending power status: {isPoweredOn}.");
-
-            using (var client = new Communication.ClientApi.Client(serverUrl))
-                await client.UpdateDevicePowerStatus(new Guid(computerDeviceId), isPoweredOn);
-
-            Logger.Log(this, LogLevel.Info, $"Sent power status: {isPoweredOn}.");
+            Application.Exit();
         }
     }
 }
---
+++ DeviceControl.WindowsEventService/HiddenForm.cs
@@ -0,0 +1,87 @@
+﻿using DeviceControl.Core;
+using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeviceControl.WindowsEventService
+{
+    // This is necessary to create a message loop in order to listen to events on SystemEvents.
+    // See https://msdn.microsoft.com/en-us/library/microsoft.win32.systemevents(v=vs.110).aspx for more details.
+
+    public partial class HiddenForm : Form
+    {
+        public HiddenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HiddenForm_Load(object sender, EventArgs e)
+        {
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
+            Logger.Log(this, LogLevel.Info, "Subscribed to power mode changed event.");
+        }
+
+        private void HiddenForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SystemEvents.PowerModeChanged -= SystemEvents_PowerModeChanged;
+            Logger.Log(this, LogLevel.Info, "Unsubscribed from power mode changed event.");
+        }
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            try
+            {
+                // wait on the task to ensure the computer doesn't go into sleep mode before it's done
+                SendStatusAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(this, LogLevel.Error, ex);
+                throw ex;
+            }
+
+            async Task SendStatusAsync()
+            {
+                switch (e.Mode)
+                {
+                    case PowerModes.Resume:
+                        await PowerStatus.SendAsync(true);
+                        break;
+                    case PowerModes.Suspend:
+                        await PowerStatus.SendAsync(false);
+                        break;
+                }
+            }
+        }
+    }
+
+    partial class HiddenForm
+    {
+        private IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                components?.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(0, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "HiddenForm";
+            Text = "HiddenForm";
+            WindowState = FormWindowState.Minimized;
+            Load += new EventHandler(HiddenForm_Load);
+            FormClosing += new FormClosingEventHandler(HiddenForm_FormClosing);
+            ResumeLayout(false);
+        }
+    }
+}
