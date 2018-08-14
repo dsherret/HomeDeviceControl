7a9abaaa891785b4eca045e0f03d85b8aafe9d01
David Sherret <dsherret@gmail.com>
2018-08-13 20:44:57 -0400

    Separate out PowerEventListener from HiddenForm.

--- Communication/WindowsEventService/HiddenForm.cs
+++ Communication/WindowsEventService/HiddenForm.cs
@@ -1,8 +1,6 @@
-﻿using HomeDeviceControl.Core;
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Management;
 using System.Windows.Forms;
 
 namespace HomeDeviceControl.Communication.WindowsEventService
@@ -12,8 +10,7 @@ namespace HomeDeviceControl.Communication.WindowsEventService
 
     public partial class HiddenForm : Form
     {
-        private ManagementEventWatcher managementEventWatcher;
-
+        private PowerEventListener _powerEventListener;
         public HiddenForm()
         {
             InitializeComponent();
@@ -21,44 +18,13 @@ namespace HomeDeviceControl.Communication.WindowsEventService
 
         private void HiddenForm_Load(object sender, EventArgs e)
         {
-            Logger.Log(this, LogLevel.Info, "Subscribed to power mode changed event.");
-
-            // from here: https://stackoverflow.com/a/9159974/188246
-            // much more reliable than SystemEvents.PowerModeChanged
-            var query = new WqlEventQuery();
-            var scope = new ManagementScope("root\\CIMV2");
-
-            query.EventClassName = "Win32_PowerManagementEvent";
-            managementEventWatcher = new ManagementEventWatcher(scope, query);
-            managementEventWatcher.EventArrived += PowerEventArrived;
-            managementEventWatcher.Start();
-
-            Logger.Log(this, LogLevel.Info, "Subscribed to power mode changed event.");
+            _powerEventListener = new PowerEventListener();
+            _powerEventListener.Start();
         }
 
         private void HiddenForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            managementEventWatcher?.Stop();
-            Logger.Log(this, LogLevel.Info, "Unsubscribed from power mode changed event.");
-        }
-
-        private async void PowerEventArrived(object sender, EventArrivedEventArgs e)
-        {
-            const string SUSPEND_EVENT = "4";
-            const string RESUME_EVENT = "7";
-
-            foreach (PropertyData pd in e.NewEvent.Properties)
-            {
-                switch (pd?.Value?.ToString())
-                {
-                    case SUSPEND_EVENT:
-                        await PowerStatus.SendAsync(false);
-                        break;
-                    case RESUME_EVENT:
-                        await PowerStatus.SendAsync(true);
-                        break;
-                }
-            }
+            _powerEventListener?.Stop();
         }
     }
 
---
+++ Communication/WindowsEventService/PowerEventListener.cs
@@ -0,0 +1,63 @@
+﻿using HomeDeviceControl.Core;
+using System.Management;
+
+namespace HomeDeviceControl.Communication.WindowsEventService
+{
+    public class PowerEventListener
+    {
+        private readonly ManagementEventWatcher managementEventWatcher;
+        private bool _isSubscribed = false;
+
+        public PowerEventListener()
+        {
+            // from here: https://stackoverflow.com/a/9159974/188246
+            // much more reliable than SystemEvents.PowerModeChanged
+            var query = new WqlEventQuery();
+            var scope = new ManagementScope("root\\CIMV2");
+
+            query.EventClassName = "Win32_PowerManagementEvent";
+            managementEventWatcher = new ManagementEventWatcher(scope, query);
+        }
+
+        public void Start()
+        {
+            if (_isSubscribed)
+                return;
+
+            managementEventWatcher.EventArrived += PowerEventArrived;
+            managementEventWatcher.Start();
+            _isSubscribed = true;
+            Logger.Log(this, LogLevel.Info, "Subscribed to power mode changed event.");
+        }
+
+        public void Stop()
+        {
+            if (!_isSubscribed)
+                return;
+
+            managementEventWatcher.EventArrived -= PowerEventArrived;
+            managementEventWatcher.Stop();
+            _isSubscribed = false;
+            Logger.Log(this, LogLevel.Info, "Unsubscribed from power mode changed event.");
+        }
+
+        private async void PowerEventArrived(object sender, EventArrivedEventArgs e)
+        {
+            const string SUSPEND_EVENT = "4";
+            const string RESUME_EVENT = "7";
+
+            foreach (PropertyData pd in e.NewEvent.Properties)
+            {
+                switch (pd?.Value?.ToString())
+                {
+                    case SUSPEND_EVENT:
+                        await PowerStatus.SendAsync(false);
+                        break;
+                    case RESUME_EVENT:
+                        await PowerStatus.SendAsync(true);
+                        break;
+                }
+            }
+        }
+    }
+}
