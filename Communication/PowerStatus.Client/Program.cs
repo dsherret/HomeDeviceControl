fd5602fcf48be9981ea5c3a04ec6b19f44c6b3ff
David Sherret <dsherret@gmail.com>
2018-08-14 19:27:33 -0400

    PowerStatus.Client - Remove hard coded url.

--- Communication/PowerStatus.Client/Program.cs
+++ Communication/PowerStatus.Client/Program.cs
@@ -7,9 +7,9 @@ namespace HomeDeviceControl.Communication.PowerStatus.Client
     {
         static async Task<int> Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 3)
             {
-                Console.WriteLine("Error: Expected two arguments.");
+                Console.WriteLine("Error: Expected three arguments.");
                 PrintUsage();
                 return 1;
             }
@@ -28,8 +28,7 @@ namespace HomeDeviceControl.Communication.PowerStatus.Client
                 return 1;
             }
 
-            // todo: don't hardcode this url
-            using (var client = new ClientApi.Client("http://192.168.1.125:8084"))
+            using (var client = new ClientApi.Client(args[2]))
                 await client.UpdateDevicePowerStatus(deviceId, isPoweredOn);
 
             return 0;
@@ -41,8 +40,11 @@ namespace HomeDeviceControl.Communication.PowerStatus.Client
             Console.WriteLine("=====");
             Console.WriteLine("Usage");
             Console.WriteLine("=====");
-            Console.WriteLine("The application requires one guid argument for the device id and one boolean parameter to say the power status.");
-            Console.WriteLine("Example: HomeDeviceControl.Communication.PowerStatus.Client.exe 09E9E275-5C73-4FD0-B44B-D6890B176B75 true");
+            Console.WriteLine("The application requires three arguments:");
+            Console.WriteLine("  * Guid argument for the device id");
+            Console.WriteLine("  * Boolean to say the power status.");
+            Console.WriteLine("  * Url to server root.");
+            Console.WriteLine("Example: HomeDeviceControl.Communication.PowerStatus.Client.exe 09E9E275-5C73-4FD0-B44B-D6890B176B75 true http://192.168.1.125:8084");
         }
     }
 }
