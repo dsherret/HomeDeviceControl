d32ea5232305141d952a3157f4f453c8d85dd169
David Sherret <dsherret@gmail.com>
2018-08-27 18:46:02 -0400

    fix: #1 - Retry sending power status if failed.

--- Communication/PowerStatus.WindowsService/PowerStatus.cs
+++ Communication/PowerStatus.WindowsService/PowerStatus.cs
@@ -8,18 +8,32 @@ namespace HomeDeviceControl.Communication.PowerStatus.WindowsService
     {
         public static async Task SendAsync(bool isPoweredOn)
         {
-            Logger.Log(typeof(PowerStatus), LogLevel.Info, $"Sending power status: {isPoweredOn}.");
-
-            try
+            // todo: abstract out retrying
+            const int MAX_RETRIES = 10;
+            for (var i = 0; i < MAX_RETRIES; i++)
             {
-                using (var client = new ClientApi.Client(Settings.Default.ServerUrl))
-                    await client.UpdateDevicePowerStatus(Guid.Parse(Settings.Default.ComputerDeviceId), isPoweredOn);
-
-                Logger.Log(typeof(PowerStatus), LogLevel.Info, $"Sent power status: {isPoweredOn}.");
+                if (await SendInternalAsync())
+                    return;
+                await Task.Delay(100 * (i + 1));
             }
-            catch (Exception ex)
+
+            async Task<bool> SendInternalAsync()
             {
-                Logger.Log(typeof(PowerStatus), LogLevel.Error, "Problem sending power status.", ex);
+                Logger.Log(typeof(PowerStatus), LogLevel.Info, $"Sending power status: {isPoweredOn}.");
+
+                try
+                {
+                    using (var client = new ClientApi.Client(Settings.Default.ServerUrl))
+                        await client.UpdateDevicePowerStatus(Guid.Parse(Settings.Default.ComputerDeviceId), isPoweredOn);
+
+                    Logger.Log(typeof(PowerStatus), LogLevel.Info, $"Sent power status: {isPoweredOn}.");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(typeof(PowerStatus), LogLevel.Error, "Problem sending power status.", ex);
+                    return false;
+                }
             }
         }
     }
